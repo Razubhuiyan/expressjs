55da7955d5d65a61c83269679f75f53d7b2c1262
Razu Bhuiyan <razubn1994@gmail.com>
2021-04-09 20:16:28 +0600

    covered express raw,urlencoded,static function

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const express = require('express');
 
 const app = express();
 
+//app.use(express.json());
+//app.use(express.raw());
+//app.use(express.text());
+//app.use(express.urlencoded());
+app.use(express.static(`${__dirname}/public/`, {
+    index: "index.html"
+}));
+
 app.get('/', (req, res) => {
     res.send("This is the home page");
 })
 
 app.post('/', (req, res) => {
-    console.log(req.body);
+    //console.log(req.body);
     res.send("This is home page with post request");
 });
 
