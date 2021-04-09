9d8aec314cf119cc47407dbdffcccb89afd7d648
Razu Bhuiyan <razubn1994@gmail.com>
2021-04-09 20:33:16 +0600

    coverd express.Router function

--- index.js
+++ index.js
@@ -3,19 +3,24 @@ const express = require('express');
 
 const app = express();
 
+const router = express.Router({
+    caseSensitive: true
+});
 //app.use(express.json());
 //app.use(express.raw());
 //app.use(express.text());
 //app.use(express.urlencoded());
-app.use(express.static(`${__dirname}/public/`, {
-    index: "index.html"
-}));
+//app.use(express.static(`${__dirname}/public/`, {
+//    index: "index.html"
+//}));
 
-app.get('/', (req, res) => {
+app.use(router);
+
+router.get('/about', (req, res) => {
     res.send("This is the home page");
 })
 
-app.post('/', (req, res) => {
+router.post('/', (req, res) => {
     //console.log(req.body);
     res.send("This is home page with post request");
 });
