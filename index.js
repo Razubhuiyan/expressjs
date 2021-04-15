27e981c80dd5fe658cbb34231cab9bfde2daf9d0
Razu Bhuiyan <razubn1994@gmail.com>
2021-04-15 21:31:34 +0600

    Covered express app object

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
 const express = require('express');
 
-
 const app = express();
 
-const router = express.Router({
-    caseSensitive: true
-});
-//app.use(express.json());
-//app.use(express.raw());
-//app.use(express.text());
-//app.use(express.urlencoded());
-//app.use(express.static(`${__dirname}/public/`, {
-//    index: "index.html"
-//}));
+app.set('view engine', 'ejs');
 
-app.use(router);
-
-router.get('/about', (req, res) => {
-    res.send("This is the home page");
-})
-
-router.post('/', (req, res) => {
-    //console.log(req.body);
-    res.send("This is home page with post request");
-});
-
-
+app.route('/about/mission')
+    .get((req, res) => {
+        res.render('pages/about');
+    })
+    .post((req, res) => {
+        res.send('This is the application home page with post request');
+    })
+    .put((req, res) => {
+        res.send('This is the application home page with put request');
+    });
 
 app.listen(3000, () => {
-    console.log("Server listening in port 3000");
+    console.log('Server listening in port 3000');
 });
